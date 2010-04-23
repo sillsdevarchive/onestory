23230a487fbbb6b842dbf29d4b071cd979cfd718
Bob Eaton <bob_barb_eaton@yahoo.com>
2010-04-22 19:36:23 -0500

    forgot to check in the ViewEnableForm (for selecting which fields to show)

---
+++ StoryEditor/ViewEnableForm.Designer.cs
@@ -0,0 +1,215 @@
+namespace OneStoryProjectEditor
+{
+    partial class ViewEnableForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ViewEnableForm));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.checkBoxLangVernacular = new System.Windows.Forms.CheckBox();
+            this.checkBoxLangNationalBT = new System.Windows.Forms.CheckBox();
+            this.checkBoxLangInternationalBT = new System.Windows.Forms.CheckBox();
+            this.checkBoxAnchors = new System.Windows.Forms.CheckBox();
+            this.checkBoxStoryTestingQuestions = new System.Windows.Forms.CheckBox();
+            this.checkBoxRetellings = new System.Windows.Forms.CheckBox();
+            this.checkBoxConsultantNotes = new System.Windows.Forms.CheckBox();
+            this.checkBoxCoachNotes = new System.Windows.Forms.CheckBox();
+            this.checkBoxBibleViewer = new System.Windows.Forms.CheckBox();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.Controls.Add(this.checkBoxLangVernacular);
+            this.flowLayoutPanel1.Controls.Add(this.checkBoxLangNationalBT);
+            this.flowLayoutPanel1.Controls.Add(this.checkBoxLangInternationalBT);
+            this.flowLayoutPanel1.Controls.Add(this.checkBoxAnchors);
+            this.flowLayoutPanel1.Controls.Add(this.checkBoxStoryTestingQuestions);
+            this.flowLayoutPanel1.Controls.Add(this.checkBoxRetellings);
+            this.flowLayoutPanel1.Controls.Add(this.checkBoxConsultantNotes);
+            this.flowLayoutPanel1.Controls.Add(this.checkBoxCoachNotes);
+            this.flowLayoutPanel1.Controls.Add(this.checkBoxBibleViewer);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 13);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(206, 231);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // checkBoxLangVernacular
+            // 
+            this.checkBoxLangVernacular.AutoSize = true;
+            this.checkBoxLangVernacular.Location = new System.Drawing.Point(3, 3);
+            this.checkBoxLangVernacular.Name = "checkBoxLangVernacular";
+            this.checkBoxLangVernacular.Size = new System.Drawing.Size(101, 17);
+            this.checkBoxLangVernacular.TabIndex = 0;
+            this.checkBoxLangVernacular.Text = "LangVernacular";
+            this.checkBoxLangVernacular.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxLangNationalBT
+            // 
+            this.checkBoxLangNationalBT.AutoSize = true;
+            this.checkBoxLangNationalBT.Location = new System.Drawing.Point(3, 26);
+            this.checkBoxLangNationalBT.Name = "checkBoxLangNationalBT";
+            this.checkBoxLangNationalBT.Size = new System.Drawing.Size(103, 17);
+            this.checkBoxLangNationalBT.TabIndex = 1;
+            this.checkBoxLangNationalBT.Text = "LangNationalBT";
+            this.checkBoxLangNationalBT.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxLangInternationalBT
+            // 
+            this.checkBoxLangInternationalBT.AutoSize = true;
+            this.checkBoxLangInternationalBT.Location = new System.Drawing.Point(3, 49);
+            this.checkBoxLangInternationalBT.Name = "checkBoxLangInternationalBT";
+            this.checkBoxLangInternationalBT.Size = new System.Drawing.Size(165, 17);
+            this.checkBoxLangInternationalBT.TabIndex = 2;
+            this.checkBoxLangInternationalBT.Text = "&English back translation fields";
+            this.checkBoxLangInternationalBT.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAnchors
+            // 
+            this.checkBoxAnchors.AutoSize = true;
+            this.checkBoxAnchors.Location = new System.Drawing.Point(3, 72);
+            this.checkBoxAnchors.Name = "checkBoxAnchors";
+            this.checkBoxAnchors.Size = new System.Drawing.Size(65, 17);
+            this.checkBoxAnchors.TabIndex = 3;
+            this.checkBoxAnchors.Text = "&Anchors";
+            this.checkBoxAnchors.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxStoryTestingQuestions
+            // 
+            this.checkBoxStoryTestingQuestions.AutoSize = true;
+            this.checkBoxStoryTestingQuestions.Location = new System.Drawing.Point(3, 95);
+            this.checkBoxStoryTestingQuestions.Name = "checkBoxStoryTestingQuestions";
+            this.checkBoxStoryTestingQuestions.Size = new System.Drawing.Size(132, 17);
+            this.checkBoxStoryTestingQuestions.TabIndex = 4;
+            this.checkBoxStoryTestingQuestions.Text = "Story &testing questions";
+            this.checkBoxStoryTestingQuestions.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxRetellings
+            // 
+            this.checkBoxRetellings.AutoSize = true;
+            this.checkBoxRetellings.Location = new System.Drawing.Point(3, 118);
+            this.checkBoxRetellings.Name = "checkBoxRetellings";
+            this.checkBoxRetellings.Size = new System.Drawing.Size(72, 17);
+            this.checkBoxRetellings.TabIndex = 5;
+            this.checkBoxRetellings.Text = "&Retellings";
+            this.checkBoxRetellings.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxConsultantNotes
+            // 
+            this.checkBoxConsultantNotes.AutoSize = true;
+            this.checkBoxConsultantNotes.Location = new System.Drawing.Point(3, 141);
+            this.checkBoxConsultantNotes.Name = "checkBoxConsultantNotes";
+            this.checkBoxConsultantNotes.Size = new System.Drawing.Size(105, 17);
+            this.checkBoxConsultantNotes.TabIndex = 6;
+            this.checkBoxConsultantNotes.Text = "&Consultant notes";
+            this.checkBoxConsultantNotes.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxCoachNotes
+            // 
+            this.checkBoxCoachNotes.AutoSize = true;
+            this.checkBoxCoachNotes.Location = new System.Drawing.Point(3, 164);
+            this.checkBoxCoachNotes.Name = "checkBoxCoachNotes";
+            this.checkBoxCoachNotes.Size = new System.Drawing.Size(86, 17);
+            this.checkBoxCoachNotes.TabIndex = 7;
+            this.checkBoxCoachNotes.Text = "Coac&h notes";
+            this.checkBoxCoachNotes.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxBibleViewer
+            // 
+            this.checkBoxBibleViewer.AutoSize = true;
+            this.checkBoxBibleViewer.Location = new System.Drawing.Point(3, 187);
+            this.checkBoxBibleViewer.Name = "checkBoxBibleViewer";
+            this.checkBoxBibleViewer.Size = new System.Drawing.Size(83, 17);
+            this.checkBoxBibleViewer.TabIndex = 8;
+            this.checkBoxBibleViewer.Text = "&Bible viewer";
+            this.checkBoxBibleViewer.UseVisualStyleBackColor = true;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOK.Location = new System.Drawing.Point(31, 257);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 1;
+            this.buttonOK.Text = "&OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(124, 257);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 2;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // ViewEnableForm
+            // 
+            this.AcceptButton = this.buttonOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(231, 292);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ViewEnableForm";
+            this.Text = "Check Fields to Make Visible";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.CheckBox checkBoxLangVernacular;
+        private System.Windows.Forms.CheckBox checkBoxLangNationalBT;
+        private System.Windows.Forms.CheckBox checkBoxLangInternationalBT;
+        private System.Windows.Forms.CheckBox checkBoxAnchors;
+        private System.Windows.Forms.CheckBox checkBoxStoryTestingQuestions;
+        private System.Windows.Forms.CheckBox checkBoxRetellings;
+        private System.Windows.Forms.CheckBox checkBoxConsultantNotes;
+        private System.Windows.Forms.CheckBox checkBoxCoachNotes;
+        private System.Windows.Forms.CheckBox checkBoxBibleViewer;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ StoryEditor/ViewEnableForm.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OneStoryProjectEditor
+{
+	public partial class ViewEnableForm : Form
+	{
+		public ViewEnableForm(StoryEditor theSE,
+			ProjectSettings projSettings, StoryData theCurrentStory)
+		{
+			InitializeComponent();
+			if (projSettings.Vernacular.HasData)
+			{
+				checkBoxLangVernacular.Text = String.Format(Properties.Resources.IDS_LanguageFields,
+															projSettings.Vernacular.LangName);
+			}
+			else
+				checkBoxLangVernacular.Visible = false;
+
+			if (projSettings.NationalBT.HasData)
+			{
+				checkBoxLangNationalBT.Text = String.Format(Properties.Resources.IDS_StoryLanguageField,
+															projSettings.NationalBT.LangName);
+
+				checkBoxLangNationalBT.Enabled = ((theCurrentStory != null)
+												  && (((int) theCurrentStory.ProjStage.ProjectStage)
+													  >= (int) StoryStageLogic.ProjectStages.eProjFacTypeNationalBT));
+			}
+			else
+				checkBoxLangNationalBT.Visible = false;
+
+			if (projSettings.InternationalBT.HasData)
+			{
+				checkBoxLangInternationalBT.Visible = true;
+				checkBoxLangInternationalBT.Enabled = ((theCurrentStory != null)
+													   && (((int) theCurrentStory.ProjStage.ProjectStage)
+														   >=
+														   (int)
+														   StoryStageLogic.ProjectStages.eProjFacTypeInternationalBT));
+			}
+			else
+				checkBoxLangInternationalBT.Visible = false;
+
+			checkBoxAnchors.Visible = ((theCurrentStory != null)
+									   && (((int) theCurrentStory.ProjStage.ProjectStage)
+										   >= (int) StoryStageLogic.ProjectStages.eProjFacAddAnchors));
+
+			checkBoxStoryTestingQuestions.Visible = ((theCurrentStory != null)
+													 && (((int) theCurrentStory.ProjStage.ProjectStage)
+														 > (int) StoryStageLogic.ProjectStages.eProjFacAddStoryQuestions));
+
+
+			checkBoxConsultantNotes.Enabled =
+				checkBoxCoachNotes.Enabled = (theCurrentStory != null);
+		}
+
+		public VerseData.ViewItemToInsureOn ItemsToInsureAreOn
+		{
+			set
+			{
+				if (VerseData.IsViewItemOn(value, VerseData.ViewItemToInsureOn.eVernacularLangField))
+					checkBoxLangVernacular.Checked = true;
+				if (VerseData.IsViewItemOn(value, VerseData.ViewItemToInsureOn.eNationalLangField))
+					checkBoxLangNationalBT.Checked = true;
+				if (VerseData.IsViewItemOn(value, VerseData.ViewItemToInsureOn.eEnglishBTField))
+					checkBoxLangInternationalBT.Checked = true;
+				if (VerseData.IsViewItemOn(value, VerseData.ViewItemToInsureOn.eAnchorFields))
+					checkBoxAnchors.Checked = true;
+				if (VerseData.IsViewItemOn(value, VerseData.ViewItemToInsureOn.eStoryTestingQuestionFields))
+					checkBoxStoryTestingQuestions.Checked = true;
+				if (VerseData.IsViewItemOn(value, VerseData.ViewItemToInsureOn.eRetellingFields))
+					checkBoxRetellings.Checked = true;
+				if (VerseData.IsViewItemOn(value, VerseData.ViewItemToInsureOn.eConsultantNoteFields))
+					checkBoxConsultantNotes.Checked = true;
+				if (VerseData.IsViewItemOn(value, VerseData.ViewItemToInsureOn.eCoachNotesFields))
+					checkBoxCoachNotes.Checked = true;
+				if (VerseData.IsViewItemOn(value, VerseData.ViewItemToInsureOn.eBibleViewer))
+					checkBoxBibleViewer.Checked = true;
+			}
+
+			get
+			{
+				return VerseData.SetItemsToInsureOn(
+					checkBoxLangVernacular.Checked,
+					checkBoxLangNationalBT.Checked,
+					checkBoxLangInternationalBT.Checked,
+					checkBoxAnchors.Checked,
+					checkBoxStoryTestingQuestions.Checked,
+					checkBoxRetellings.Checked,
+					checkBoxConsultantNotes.Checked,
+					checkBoxCoachNotes.Checked,
+					checkBoxBibleViewer.Checked);
+			}
+		}
+
+		private void buttonOK_Click(object sender, EventArgs e)
+		{
+			DialogResult = DialogResult.OK;
+			Close();
+		}
+	}
+}
