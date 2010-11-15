5794a48665ff8e614f89d431c28ff7c4c76351c0
Bob Eaton <bob_barb_eaton@yahoo.com>
2010-11-15 15:09:34 +0530

    update the website to v23 which includes fixes for Toolbox export of hidden lines, splitting stories and scrolling the box to the top of the screen when adding new connotes

--- StoryEditor/Properties/AssemblyInfo.cs
+++ StoryEditor/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.22")]
+[assembly: AssemblyFileVersion("1.4.0.23")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
