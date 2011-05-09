a6129ac7fd25c912e5b743142e617128949b3f8a
Bob Eaton <bob_barb_eaton@yahoo.com>
2011-05-09 14:50:48 -0500

    2.2.0.7 release

--- StoryEditor/Properties/AssemblyInfo.cs
+++ StoryEditor/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.6")]
+[assembly: AssemblyFileVersion("2.2.0.7")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
