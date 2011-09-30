9356cc491e11cb41cffa87015c3c1e2bc5792015
Bob Eaton <bob_barb_eaton@yahoo.com>
2011-09-29 21:41:52 -0400

    add ability to support encrypted Sword resources (since I needed to encrypt the indonesian resources)

--- StoryEditor/Properties/Settings.Designer.cs
+++ StoryEditor/Properties/Settings.Designer.cs
@@ -919,5 +919,20 @@ namespace OneStoryProjectEditor.Properties {
                 this["LastLocalizationId"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
+            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
+            "tring>KEJADIAN</string>\r\n  <string>DZnWNke9ScuB5IThEtS8txhiXMgy+5Ca</string>\r\n</" +
+            "ArrayOfString>")]
+        public global::System.Collections.Specialized.StringCollection SwordModuleToUnlockKey {
+            get {
+                return ((global::System.Collections.Specialized.StringCollection)(this["SwordModuleToUnlockKey"]));
+            }
+            set {
+                this["SwordModuleToUnlockKey"] = value;
+            }
+        }
     }
 }
