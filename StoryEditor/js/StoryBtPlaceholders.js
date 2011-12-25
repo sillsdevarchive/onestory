d94c32372901a8dff4f7e21bfb2538304ff0d404
Bob Eaton <bob_barb_eaton@yahoo.com>
2011-12-25 12:53:34 -0600

    working on StoryBtHtml (adding html and js functions). Got placeholders working

---
+++ StoryEditor/js/StoryBtPlaceholders.js
@@ -0,0 +1,12 @@
+function VernacularLanguageName() {{
+    return '{0}';
+}}
+function NationalBtLanguageName() {{
+    return '{1}';
+}}
+function InternationalBtLanguageName() {{
+    return '{2}';
+}}
+function FreeTranslationLanguageName() {{
+    return '{3}';
+}}
