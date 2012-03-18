98bcaa9154c72180b0a20c5c2e6adca2aed3622b
Bob Eaton <bob_barb_eaton@yahoo.com>
2012-03-18 18:31:46 +0530

    broke adding a new project (make sure the file exists before certain 'you must reload the file' issues come up)

--- StoryEditor/StoryEditor.cs
+++ StoryEditor/StoryEditor.cs
@@ -764,6 +764,9 @@ namespace OneStoryProjectEditor
 
 			// if we just saved, then don't bother the user anymore
 			var strProjectFilePath = StoryProject.ProjSettings.ProjectFilePath;
+			if (!File.Exists(strProjectFilePath))
+				return; // not written yet (during new project cycle)
+
 			var dateTimeCurrent = File.GetLastWriteTime(strProjectFilePath);
 			if (_dateTimeLastSaved == dateTimeCurrent)
 				return;
@@ -2467,7 +2470,7 @@ namespace OneStoryProjectEditor
 						QueryStoryPurpose();
 				}
 
-				if (_dateTimeLastSaved == DateTime.MinValue)
+				if (File.Exists(strFilename) && (_dateTimeLastSaved == DateTime.MinValue))
 				{
 					LocalizableMessageBox.Show(
 						Localizer.Str("You must reload the project before it can be saved again."), OseCaption);
