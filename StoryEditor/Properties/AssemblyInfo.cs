ed9e97596baf59314e9066117b7b4e147acad6e9
Bob Eaton <bob_barb_eaton@yahoo.com>
2012-06-23 09:26:41 -0500

    added support for referring text connote elements (precursor to full story bt pane html)

--- StoryEditor/Properties/AssemblyInfo.cs
+++ StoryEditor/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.10.1.0")]
-[assembly: AssemblyFileVersion("2.10.1.1")]
+[assembly: AssemblyFileVersion("2.10.1.2")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
