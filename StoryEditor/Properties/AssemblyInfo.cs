2d7d23eec9726c4937b35d382082a1ceaff16def
Bob Eaton <bob_barb_eaton@yahoo.com>
2012-06-29 16:31:47 -0500

    released latest changes (Saymore import and Toolbox export) as 2.10.1.4 so I can make an installer for Paul

--- StoryEditor/Properties/AssemblyInfo.cs
+++ StoryEditor/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.10.1.0")]
-[assembly: AssemblyFileVersion("2.10.1.3")]
+[assembly: AssemblyFileVersion("2.10.1.4")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
