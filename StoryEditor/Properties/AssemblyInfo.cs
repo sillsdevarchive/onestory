e2e72b01fe29cc3bf7f1445696bb435749575258
Bob <Bob@Bob-Xps17.SMRouter>
2012-10-29 14:43:58 +0530

    released fix for right-click clobbers data with transliteration update

--- StoryEditor/Properties/AssemblyInfo.cs
+++ StoryEditor/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.2.0")]
-[assembly: AssemblyFileVersion("3.0.2.2")]
+[assembly: AssemblyFileVersion("3.0.2.3")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
